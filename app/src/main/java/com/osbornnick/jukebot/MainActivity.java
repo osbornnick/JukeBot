242036e37a5d18c6aa38b03003609370e8f84628
Felix <77823268+felixny@users.noreply.github.com>
2022-07-17 22:48:18 -0400

    added home button on MainActivity.java

--- app/src/main/java/com/osbornnick/jukebot/MainActivity.java
+++ app/src/main/java/com/osbornnick/jukebot/MainActivity.java
@@ -2,7 +2,9 @@ package com.osbornnick.jukebot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // temp code
+    public void Home(View view){
+        Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+        startActivity(intent);
     }
 }
