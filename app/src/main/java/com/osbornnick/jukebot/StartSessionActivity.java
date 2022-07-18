7010340e6b838223b904ac20c5abe1beac0037fc
Felix <77823268+felixny@users.noreply.github.com>
2022-07-17 21:13:02 -0400

    added StartSessionActivity.java

---
+++ app/src/main/java/com/osbornnick/jukebot/StartSessionActivity.java
@@ -0,0 +1,14 @@
+package com.osbornnick.jukebot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StartSessionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_session);
+    }
+}
