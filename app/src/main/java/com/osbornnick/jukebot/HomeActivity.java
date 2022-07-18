b65bc6d8a88c0ea9c0a74ffddcc91fc9c25e0098
Felix <77823268+felixny@users.noreply.github.com>
2022-07-17 23:01:35 -0400

    made start session text clickable on HomeActivity.java

--- app/src/main/java/com/osbornnick/jukebot/HomeActivity.java
+++ app/src/main/java/com/osbornnick/jukebot/HomeActivity.java
@@ -2,13 +2,29 @@ package com.osbornnick.jukebot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        TextView tv_play = (TextView) findViewById(R.id.start_session);
+
+        tv_play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, StartSessionActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
