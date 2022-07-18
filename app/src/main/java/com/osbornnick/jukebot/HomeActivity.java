29f4f0aea52a4bd16b92711c22b5a867bacf6c8d
Felix <77823268+felixny@users.noreply.github.com>
2022-07-17 23:09:03 -0400

    added PersonalSettingsActivity.java

--- app/src/main/java/com/osbornnick/jukebot/HomeActivity.java
+++ app/src/main/java/com/osbornnick/jukebot/HomeActivity.java
@@ -3,8 +3,10 @@ package com.osbornnick.jukebot;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
@@ -16,6 +18,7 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         TextView tv_play = (TextView) findViewById(R.id.start_session);
+        ImageView img_settings = (ImageView) findViewById(R.id.personal_settings);
 
         tv_play.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,6 +27,16 @@ public class HomeActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        img_settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, PersonalSettingsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 
 
