960289cdb71dd112862165dd45bd4a82a19e5991
Felix <77823268+felixny@users.noreply.github.com>
2022-07-20 13:23:45 -0400

    added getUserProfile method

--- app/src/main/java/com/osbornnick/jukebot/HomeActivity.java
+++ app/src/main/java/com/osbornnick/jukebot/HomeActivity.java
@@ -2,9 +2,12 @@ package com.osbornnick.jukebot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
@@ -14,6 +17,12 @@ import com.spotify.sdk.android.auth.AuthorizationClient;
 import com.spotify.sdk.android.auth.AuthorizationRequest;
 import com.spotify.sdk.android.auth.AuthorizationResponse;
 
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class HomeActivity extends AppCompatActivity {
 
     private static final String TAG = "HomeActivity";
@@ -21,6 +30,10 @@ public class HomeActivity extends AppCompatActivity {
     private static final String AUTH_TOKEN = "AUTH_TOKEN";
     private static final String CLIENT_ID = "690520ea8148443da28b0dd4555c8ef2";
     private static final String REDIRECT_URI = "com.jukebot://callback";
+    private URL mUrl = null;
+    private InputStream mInputStream = null;
+    private HttpURLConnection conn = null;
+    private String token = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,6 +76,8 @@ public class HomeActivity extends AppCompatActivity {
             switch (response.getType()){
                 case TOKEN:
                     Log.d(TAG, "onActivityResult: " + response.getAccessToken());
+                    token = response.getAccessToken();
+                    getUserProfile();
                     Intent intent = new Intent(HomeActivity.this, StartSessionActivity.class);
                     intent.putExtra(AUTH_TOKEN, response.getAccessToken());
                     startActivity(intent);
@@ -81,5 +96,31 @@ public class HomeActivity extends AppCompatActivity {
         HomeActivity.this.finish();
     }
 
+    public void getUserProfile() {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+
+        Handler handler = new Handler(Looper.getMainLooper());
+
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                //Background work here
+                doInBackground("https://api.spotify.com/v1/me");
+
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        //UI Thread work here
+                        //onPostExecute(result);
+                    }
+                });
+            }
+        });
+    }
+
+    public String doInBackground(String... strings){
+
+        return "";
+    }
 
 }
