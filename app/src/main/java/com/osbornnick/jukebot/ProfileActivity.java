54e91fdbaac2f599d69fe554de22426214e38c7d
Felix <77823268+felixny@users.noreply.github.com>
2022-08-08 16:54:27 -0400

    implemented backend for ProfileActivity.java

--- app/src/main/java/com/osbornnick/jukebot/ProfileActivity.java
+++ app/src/main/java/com/osbornnick/jukebot/ProfileActivity.java
@@ -1,32 +1,53 @@
 package com.osbornnick.jukebot;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class ProfileActivity extends AppCompatActivity {
     private static final String TAG = "ProfileActivity";
+
+    TextView txt_username, txt_date;
+    FirebaseFirestore db;
+    FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        TextView name = findViewById(R.id.txt_usernameDisplay);
-        TextView email = findViewById(R.id.txt_emailDisplay);
-        TextView country = findViewById(R.id.txt_countryDisplay);
+        db = FirebaseFirestore.getInstance();
+        txt_username = findViewById(R.id.txt_username);
+        txt_date = findViewById(R.id.txt_date);
 
-        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
-        String username = preferences.getString("Name", "");
-        String spotify_email = preferences.getString("Email","");
-        String countryCode = preferences.getString("Country","");
+        getUserInfo();
+    }
 
-        name.setText(username);
-        email.setText(spotify_email);
-        country.setText(countryCode);
+    private void getUserInfo(){
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        db.collection("users").document(user.getUid()).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful() && task.getResult() != null) {
+                    String username = task.getResult().getString("username");
+                    String date = task.getResult().getTimestamp("dateCreated").toDate().toString();
+                    txt_username.setText(username);
+                    txt_date.setText(date);
 
+                }
+            }
+        });
     }
 }
