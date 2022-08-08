d42f87bc900d5b097545f8d9bb0d42b87ab71ee0
Felix <77823268+felixny@users.noreply.github.com>
2022-08-08 14:47:07 -0400

    StartSessionActivity.java backend implemented

--- app/src/main/java/com/osbornnick/jukebot/StartSessionActivity.java
+++ app/src/main/java/com/osbornnick/jukebot/StartSessionActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -12,6 +13,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -23,6 +25,8 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 public class StartSessionActivity extends AppCompatActivity {
     FirebaseUser user;
@@ -36,6 +40,8 @@ public class StartSessionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_session);
+
+        // initialize UI elements
         mSessionName = findViewById(R.id.session_Name);
         btn_confirm = findViewById(R.id.btn_confirm);
         btn_cancel = findViewById(R.id.btn_cancel);
@@ -48,30 +54,7 @@ public class StartSessionActivity extends AppCompatActivity {
         Log.d(TAG, "onCreate: " + uid);
 
 
-        mSessionName.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if (s.length() == 0){
-                    btn_confirm.setClickable(false);
-                } else {
-                    btn_confirm.setClickable(true);
-                }
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-
-
-        });
-
+        // cancel btn brings up to previous activity
         btn_cancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -79,13 +62,45 @@ public class StartSessionActivity extends AppCompatActivity {
             }
         });
 
+        // confirm opens new activity
         btn_confirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                if (mSessionName.getText().length() == 0){
+                    Toast.makeText(StartSessionActivity.this,"Please enter session name", Toast.LENGTH_SHORT).show();
+                }
+                String sessionName = mSessionName.getText().toString();
+                addSessionName(sessionName);
+                Log.d(TAG, "onClick: " + sessionName);
+                //mSessionName.setText("");
+                Intent intent = new Intent(StartSessionActivity.this, SessionAdminActivity.class);
+                intent.putExtra("session_id", user.getUid());
+                intent.putExtra("session_name", mSessionName.getText());
+                startActivity(intent);
             }
         });
 
+
+    }
+
+    // add session name to the database
+    private void addSessionName(String sessionName){
+        Map<String, Object> userData = new HashMap<>();
+        userData.put("name", sessionName);
+        userData.put("adminUser", user.getUid());
+        userData.put("allowChat", true);
+        userData.put("allowInvite", true);
+        userData.put("allowJoins", true);
+
+        // add session name
+        db.collection("Session").document(user.getUid()).set(userData);
+
+        Map<String, Object> queueData = new HashMap<>();
+        queueData.put("deleted",true);
+        queueData.put("played",false);
+        queueData.put("playing",false);
+        // set queue collection
+        db.collection("Session").document(user.getUid()).collection("queue").document("initialSong").set(queueData);
     }
 
 
